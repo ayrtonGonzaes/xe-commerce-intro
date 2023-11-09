63d911c3064df5da5152028a122998eb137f0226
ayrtonGonzales <ayrtongonzales-2003@outlook.com>
2023-11-09 11:42:24 -0300

    Cree un input con un mensaje de error personalizado

--- js/segundaIntro.js
+++ js/segundaIntro.js
@@ -12,3 +12,32 @@ window.addEventListener("load", () =>{
         footer.classList.add("hidden");
    })
 });
+
+
+function validarTexto() {
+     let inputElement = document.getElementById("inputTexto");
+     let mensajeErrorElement = document.getElementById("mensajeError");
+ 
+     // Validar el valor del input (solo texto, sin números ni caracteres especiales)
+     let valorInput = inputElement.value;
+     if (!/^[a-zA-Z\s]+$/.test(valorInput)) {
+         mensajeErrorElement.textContent = "Por favor, ingresa un texto válido (sin números ni caracteres especiales).";
+         mensajeErrorElement.style.display = "block"; // Muestra el mensaje de error
+         inputElement.classList.add("invalid-input");
+     } else {
+         mensajeErrorElement.textContent = ""; // Limpiar el mensaje de error si el valor es válido
+         mensajeErrorElement.style.display = "none"; // Oculta el mensaje de error
+         inputElement.classList.remove("invalid-input");
+     }
+}
+ 
+ // Agregar evento clic al documento para ocultar el mensaje de error al hacer clic fuera del input
+document.addEventListener("click", function(event) {
+     let inputElement = document.getElementById("inputTexto");
+     let mensajeErrorElement = document.getElementById("mensajeError");
+ 
+     if (event.target !== inputElement) {
+         mensajeErrorElement.style.display = "none"; // Oculta el mensaje de error
+         inputElement.classList.remove("invalid-input");
+     }
+ });
